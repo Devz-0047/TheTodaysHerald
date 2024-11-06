c8f2bccfe4acbd222f2a3cb5809058d4fbb531c0
Devz-0047 <devendersingh00747@gmail.com>
2024-11-06 20:08:54 +0530

    Adding I18N

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useTranslation } from "react-i18next";
+
 // const API_KEY = import.meta.env.VITE_API_KEY;
 function App() {
-  return <div className="text-4xl text-gray-900 h2">Hello Web,</div>;
+  const { t } = useTranslation();
+  return <div className="text-4xl text-gray-900 h2">{t("welcomeMessage")}</div>;
 }
 
 export default App;
