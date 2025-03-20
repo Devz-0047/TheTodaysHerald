86c66cdbc1a916b035185107d175330bf20b2e00
Devz-0047 <devendersingh00747@gmail.com>
2025-03-20 16:09:22 +0530

    Correct India endpoints

--- src/components/GeneralNews.tsx
+++ src/components/GeneralNews.tsx
@@ -15,7 +15,7 @@ function GeneralNews() {
           if (!genre) return [];  // Prevent API call if genre is undefined
           const url =
             genre === "India"
-              ? `https://gnews.io/api/v4/top-headlines?token=${API_KEY}&country=in&lang=${languagevalue}`
+              ? `https://gnews.io/api/v4/top-headlines?token=${API_KEY}&country=in&lang=en`
               : `https://gnews.io/api/v4/top-headlines?category=${genre}&token=${API_KEY}&lang=${languagevalue}`;
       
           const response = await axios.get<{ articles: Article[] }>(url);
